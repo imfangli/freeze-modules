2116fea0704e83538f02f27661135925c50a1087
imfangli <fangli8080@gmail.com>
2017-05-13 11:36:04 +0800

    add comments

--- src/freeze.js
+++ src/freeze.js
@@ -2,23 +2,29 @@
 const fs = require('fs')
 const exec = require('child_process').exec
 
+// Get installed modules info
 const installedModulesPromise = getInstalledModules()
+// Get package.josn info
 const PackageDataPromise = getPackageData()
 
 console.log('Fetching data...')
 
+// When data ready...
 Promise.all([
   installedModulesPromise,
   PackageDataPromise
 ]).then(([installedModules, PackageData]) => {
+  // Update dependencie modules
   Object.keys(PackageData.dependencies).map(moduleName => {
     PackageData.dependencies[moduleName] = installedModules.dependencies[moduleName].version
   })
 
+  // Update dev dependencie modules
   Object.keys(PackageData.devDependencies).map(moduleName => {
     PackageData.devDependencies[moduleName] = installedModules.dependencies[moduleName].version
   })
 
+  // Write updated info to package.json
   writePackageData(PackageData).then(() => {
     console.log('package.json Updated!')
   })
@@ -27,6 +33,10 @@ Promise.all([
   process.exit(0)
 })
 
+/**
+ * Get installed modules info
+ * @return {Promise<Object, Error>} Installed modules info
+ */
 function getInstalledModules () {
   return new Promise((resolve, reject) => {
     exec('npm list --depth=0 --json', (error, stdout, stderr) => {
@@ -39,6 +49,10 @@ function getInstalledModules () {
   })
 }
 
+/**
+ * Read package.json file & output data as JSON
+ * @return {Promise<Object, Error>} package.json data
+ */
 function getPackageData () {
   return new Promise((resolve, reject) => {
     fs.readFile('package.json', 'utf8', (error, data) => {
@@ -46,12 +60,16 @@ function getPackageData () {
         reject(error)
         return
       }
-      var data = JSON.parse(data)
-      resolve(data)
+      resolve(JSON.parse(data))
     })
   })
 }
 
+/**
+ * Write new data to package.json
+ * @param  {Object} data package.json data
+ * @return {Promise<String, Error>} package.json as String
+ */
 function writePackageData (data) {
   return new Promise((resolve, reject) => {
     var fileData = JSON.stringify(data, null, 2)
